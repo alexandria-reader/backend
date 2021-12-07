1ee5bdb3d4962d327457382e534206cb46a880e2
Marc Hermann <59806588+mrchrmn@users.noreply.github.com>
2021-12-07 11:56:50 +0100

    a little bit of seed data

---
+++ src/model/seed.sql
@@ -0,0 +1,41 @@
+DELETE FROM users;
+DELETE FROM admins;
+DELETE FROM languages;
+DELETE FROM words;
+DELETE FROM texts;
+DELETE FROM translations;
+DELETE FROM contexts;
+DELETE FROM languagepairs;
+DELETE FROM webdictionaries;
+
+INSERT INTO users (username, password_hash, email)
+VALUES
+('test', 'pwhash', 'test@example.com');
+
+INSERT INTO languages ("name", abbreviation)
+VALUES
+('English', 'en'),
+('German', 'de'),
+('French', 'fr');
+
+INSERT INTO texts (user_id, language_id, title, "text", ts_parsed_text)
+VALUES
+(
+1, 1, 'The Little Match Girl',
+'It was so terribly cold. Snow was falling, and it was almost dark. Evening came on, the last evening of the year. In the cold and gloom a poor little girl, bareheaded and barefoot, was walking through the streets. Of course when she had left her house she''d had slippers on, but what good had they been? They were very big slippers, way too big for her, for they belonged to her mother. The little girl had lost them running across the road, where two carriages had rattled by terribly fast. One slipper she''d not been able to find again, and a boy had run off with the other, saying he could use it very well as a cradle some day when he had children of his own. And so the little girl walked on her naked feet, which were quite red and blue with the cold. In an old apron she carried several packages of matches, and she held a box of them in her hand. No one had bought any from her all day long, and no one had given her a cent.',
+to_tsvector('It was so terribly cold. Snow was falling, and it was almost dark. Evening came on, the last evening of the year. In the cold and gloom a poor little girl, bareheaded and barefoot, was walking through the streets. Of course when she had left her house she''d had slippers on, but what good had they been? They were very big slippers, way too big for her, for they belonged to her mother. The little girl had lost them running across the road, where two carriages had rattled by terribly fast. One slipper she''d not been able to find again, and a boy had run off with the other, saying he could use it very well as a cradle some day when he had children of his own. And so the little girl walked on her naked feet, which were quite red and blue with the cold. In an old apron she carried several packages of matches, and she held a box of them in her hand. No one had bought any from her all day long, and no one had given her a cent.')
+);
+
+INSERT INTO words (language_id, word, ts_parsed_word, is_compound)
+VALUES
+(1, 'of course', phraseto_tsquery('of course'), true),
+(1, 'hunger', phraseto_tsquery('hunger'), false),
+(1, 'across the road', phraseto_tsquery('across the road'),true),
+(1, 'hunger', phraseto_tsquery('hunger'), false),
+(1, 'all day long', phraseto_tsquery('all day long'), true),
+(1, 'snowflakes', phraseto_tsquery('snowflakes'), false),
+(1, 'roast goose', phraseto_tsquery('roast goose'), true),
+(1, 'bareheaded', phraseto_tsquery('bareheaded'),false),
+(1, 'rattled by', phraseto_tsquery('rattled by'), true),
+(1, 'carriages', phraseto_tsquery('carriages'), false),
+(1, 'New Year''s eve', phraseto_tsquery('New Year''s eve'), true);
---
+++ src/services/texts.ts
@@ -0,0 +1,19 @@
+import dbQuery from '../model/db-query';
+
+const getAll = async function() {
+  const FIND_ALL_TEXTS = 'SELECT * FROM texts';
+  const result = await dbQuery(FIND_ALL_TEXTS);
+  return result.rows;
+};
+
+const getOne = async function(textId: number) {
+  const FIND_TEXT = 'SELECT * FROM texts WHERE id = %L';
+  const result = await dbQuery(FIND_TEXT, textId);
+  return result.rows[1];
+};
+
+
+export default {
+  getAll,
+  getOne,
+};
