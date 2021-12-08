280309990117ca3f452c4da3e1518a172a76758a
Eamon <eamon0989@gmail.com>
2021-12-08 19:50:53 +0100

    added basic error handling

--- src/services/users.ts
+++ src/services/users.ts
@@ -8,10 +8,10 @@ export const selectAllUsers = async function() {
 };
 
 export const addNewUser = async function (user: User) {
-  console.log(user);
   const { username, passwordHash, email } = user;
 
   const ADD_USER = 'INSERT INTO users (username, password_hash, email) Values (%L, %L, %L)';
+
   const result = await dbQuery(ADD_USER, username, passwordHash, email);
   return result.rowCount > 0;
 };
