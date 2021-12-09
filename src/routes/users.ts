f67f3e38cec92c69546f0ba8c4500c3c45ad0a96
Eamon <eamon0989@gmail.com>
2021-12-09 11:54:46 +0100

    added update password route and service

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -1,9 +1,15 @@
 import bcrypt from 'bcrypt';
 import express from 'express';
-import { selectAllUsers, addNewUser } from '../services/users'; // needs to be implemented
+import { selectAllUsers, addNewUser, updateUserPassword } from '../services/users'; // needs to be implemented
 import { User } from '../types';
 
 const userRouter = express.Router();
+// const saltPassword = async function(password: string): Promise<string> {
+//   const saltRounds = 10;
+//   const passwordHash = await bcrypt.hash(password, saltRounds);
+//   return passwordHash;
+// };
+
 
 userRouter.post('/', async (req, res, next) => {
   const { username, password, email } = req.body;
@@ -33,6 +39,29 @@ userRouter.post('/', async (req, res, next) => {
 userRouter.get('/', async (_req, res) => {
   const users = await selectAllUsers();
   res.send(users);
+});
+
+// reset password
+userRouter.put('/:userId', async (req, res) => {
+  const { userId } = req.params;
+  const { password: currentPassword, newPassword } = req.body;
+
+  const updated = await updateUserPassword(userId, currentPassword, newPassword);
+  if (updated) {
+    res.send('Your password has been updated');
+  } else {
+    res.send('Passwords do not match');
+  }
+});
+
+// set known language
+userRouter.put('/:userId', async (_req, _res) => {
+
+});
+
+// add target language
+userRouter.put('/:userId', async (_req, _res) => {
+
 });
 
 export default userRouter;
