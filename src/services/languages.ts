804d9449adfe477eb5ffb90ef445418de62aa787
Marc Hermann <59806588+mrchrmn@users.noreply.github.com>
2021-12-09 12:03:58 +0100

    more webdictionary services

--- src/services/languages.ts
+++ src/services/languages.ts
@@ -96,7 +96,21 @@ const addWebdictionary = async function(languagepairId: number, name: string, ur
 };
 
 
-const getPrefWebdicts = async function getPreferredWebdictionaries(userId: number) {
+const getWebdictsByPair = async function getWebdictionariesByLanguagePair(languagepairId: number): Promise<Array<Webdictionary> | null> {
+  const DICTIONARIES_BY_PAIR: string = `
+    SELECT * FROM webdictionaries
+    WHERE language_pair_id = %s`;
+
+  const result = await dbQuery(DICTIONARIES_BY_PAIR, languagepairId);
+  if (result.rows.length === 0) {
+    return null;
+  }
+
+  return result.rows.map((dbItem: WebdictionaryDB): Webdictionary => convertWebdictionaryTypes(dbItem));
+};
+
+
+const getPrefWebdicts = async function getPreferredWebdictionaries(userId: number): Promise<Array<Webdictionary> | null> {
   const PREFERRED_WEBDICTIONARIES: string = `
     SELECT wd.id, language_pair_id, source_language_id, target_language_id, name, url FROM webdictionaries AS wd
     JOIN languagepairs AS lp ON wd.language_pair_id = lp.id
@@ -146,4 +160,5 @@ export default {
   addWebdictionary,
   getStudiedByUser,
   getPrefWebdicts,
+  getWebdictsByPair,
 };
