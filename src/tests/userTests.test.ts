1894dd017f619dc7e248b080a7a4668791a43665
Eamon <eamon0989@gmail.com>
2021-12-09 18:51:46 +0100

    added 2 basic tests

--- src/tests/userTests.test.ts
+++ src/tests/userTests.test.ts
@@ -1,7 +1,5 @@
 import supertest from 'supertest';
 import app from '../app';
-// import userRouter from '../routes/users';
-// import { User } from '../types';
 
 const api = supertest(app);
 
@@ -25,25 +23,3 @@ test('users are successfully added', async () => {
     .expect(200)
     .expect('User test user succesfully created');
 });
-
-// test('a valid note can be added', async () => {
-//   const newNote = {
-//     content: 'async/await simplifies making async calls',
-//     important: true,
-//   }
-
-//   await api
-//     .post('/api/notes')
-//     .send(newNote)
-//     .expect(200)
-//     .expect('Content-Type', /application\/json/)
-
-//   const response = await api.get('/api/notes')
-
-//   const contents = response.body.map(r => r.content)
-
-//   expect(response.body).toHaveLength(initialNotes.length + 1)
-//   expect(contents).toContain(
-//     'async/await simplifies making async calls'
-//   )
-// })
