8c68d8f3efaa71fd5bd855c6c795f4017a5fd0fe
Eamon <eamon0989@gmail.com>
2021-12-10 10:06:59 +0100

    set test db to reset on each test run

--- src/tests/userTests.test.ts
+++ src/tests/userTests.test.ts
@@ -1,25 +1,51 @@
 import supertest from 'supertest';
 import app from '../app';
+import dbQuery from '../model/db-query';
+import resetDbCommands from '../model/test-db-reset';
 
 const api = supertest(app);
 
-test('users are returned as json', async () => {
-  await api
-    .get('/api/users')
-    .expect(200)
-    .expect('Content-Type', /application\/json/);
+beforeAll(async () => {
+  resetDbCommands.forEach(async (query) => {
+    await dbQuery(query);
+  });
 });
 
-test('users are successfully added', async () => {
-  const newUser = {
-    username: 'test user',
-    password: '12345',
-    email: 'test@userRouter.com',
-  };
+describe('Testing adding users', () => {
+  test('users are returned as json', async () => {
+    await api
+      .get('/api/users')
+      .expect(200)
+      .expect('Content-Type', /application\/json/);
+  });
 
-  await api
-    .post('/api/users')
-    .send(newUser)
-    .expect(200)
-    .expect('User test user succesfully created');
+  test('users are successfully added', async () => {
+    const newUser = {
+      username: 'test user',
+      password: '12345',
+      email: 'test@userRouter.com',
+    };
+
+    await api
+      .post('/api/users')
+      .send(newUser)
+      .expect(200)
+      .expect('User test user succesfully created');
+  });
+
+  // the error handling needs work before this can be implemented
+  // test('duplicate users are not added', async () => {
+  //   const newUser = {
+  //     username: 'test user',
+  //     password: '12345',
+  //     email: 'test@userRouter.com',
+  //   };
+
+  //   await api
+  //     .post('/api/users')
+  //     .send(newUser)
+  //     .expect(200)
+  //     .expect('User test user succesfully created');
+  // });
 });
+
