3a911fe59b83103b77accd04e4177da4f5b67536
Marc Hermann <59806588+mrchrmn@users.noreply.github.com>
2021-12-13 10:47:55 +0100

    removed two extra linebreaks

--- src/model/test-db-reset.ts
+++ src/model/test-db-reset.ts
@@ -1,5 +1,4 @@
-const reset = `
-DELETE FROM webdictionaries;
+const reset = `DELETE FROM webdictionaries;
 DELETE FROM languagepairs;
 DELETE FROM contexts;
 DELETE FROM translations;
@@ -7,7 +6,6 @@ DELETE FROM texts;
 DELETE FROM words;
 DELETE FROM languages;
 DELETE FROM admins;
-DELETE FROM users;
-`.split('\n');
+DELETE FROM users;`.split('\n');
 
 export default reset;
