2a92b8f0d112d9ba7199accd50c961aa6e1f150e
Eamon <eamon0989@gmail.com>
2021-12-13 16:50:11 +0100

    test change user password, delete user

--- src/tests/users.test.ts
+++ src/tests/users.test.ts
@@ -10,11 +10,14 @@ beforeAll(async () => {
 });
 
 describe('Testing adding users', () => {
-  test('users are returned as json', async () => {
-    await api
+  test('users are returned as json, there are no users', async () => {
+    const response = await api
       .get('/api/users')
       .expect(200)
       .expect('Content-Type', /application\/json/);
+
+    const responseBody = JSON.parse(response.text);
+    expect(responseBody).toHaveLength(0);
   });
 
   test('users are successfully added', async () => {
@@ -31,6 +34,16 @@ describe('Testing adding users', () => {
       .expect('Content-Type', /application\/json/);
 
     expect(response.text).toContain('test user');
+  });
+
+  test('There is one user', async () => {
+    const response = await api
+      .get('/api/users')
+      .expect(200)
+      .expect('Content-Type', /application\/json/);
+
+    const responseBody = JSON.parse(response.text);
+    expect(responseBody).toHaveLength(1);
   });
 
   test('duplicate usernames are not added', async () => {
@@ -65,15 +78,56 @@ describe('Testing adding users', () => {
     expect(response.text).toContain('Email already in use');
   });
 
-  // test('users are successfully deleted', async () => {
-  //   const response = await api
-  //     .post('/api/users/1')
-  //     .send(newUser)
-  //     .expect(201)
-  //     .expect('Content-Type', /application\/json/);
+  test('users can change passwords', async () => {
+    const password = {
+      password: '12345',
+      newPassword: 'password',
+    };
 
-  //   expect(response.text).toContain('test user');
-  // });
+    await api
+      .put('/api/users/1')
+      .send(password)
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
+      .expect('{"message":"Your password has been updated"}');
+  });
+
+  test('users cant change passwords unless correct password is supplied', async () => {
+    const password = {
+      password: '123456',
+      newPassword: 'password',
+    };
+
+    await api
+      .put('/api/users/1')
+      .send(password)
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
+      .expect('{"message":"Incorrect password"}');
+  });
+
+  test('users are successfully deleted', async () => {
+    const password = {
+      password: 'password',
+    };
+
+    await api
+      .delete('/api/users/1')
+      .send(password)
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
+      .expect('{"message":"Your account has been deleted"}');
+  });
+
+  test('After deletion, there are no users', async () => {
+    const response = await api
+      .get('/api/users')
+      .expect(200)
+      .expect('Content-Type', /application\/json/);
+
+    const responseBody = JSON.parse(response.text);
+    expect(responseBody).toHaveLength(0);
+  });
 
   // test changing password
 });
