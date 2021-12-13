1a1216ab75554ff3b221db51dde9814e88873304
Marc Hermann <59806588+mrchrmn@users.noreply.github.com>
2021-12-13 10:43:25 +0100

    reversed order of deletions

--- src/model/test-db-reset.ts
+++ src/model/test-db-reset.ts
@@ -1,11 +1,13 @@
-const reset = `DELETE FROM users;
-DELETE FROM admins;
-DELETE FROM languages;
-DELETE FROM words;
-DELETE FROM texts;
-DELETE FROM translations;
-DELETE FROM contexts;
+const reset = `
+DELETE FROM webdictionaries;
 DELETE FROM languagepairs;
-DELETE FROM webdictionaries;`.split('\n');
+DELETE FROM contexts;
+DELETE FROM translations;
+DELETE FROM texts;
+DELETE FROM words;
+DELETE FROM languages;
+DELETE FROM admins;
+DELETE FROM users;
+`.split('\n');
 
 export default reset;
