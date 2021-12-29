9f94b67195f67d3ce24057c20d6c0c135986ee7f
Marc Hermann <59806588+mrchrmn@users.noreply.github.com>
2021-12-29 11:54:38 +0100

    adding translation also connects to user

--- src/routes/translations.ts
+++ src/routes/translations.ts
@@ -1,5 +1,5 @@
 import express from 'express';
-import translation from '../services/translations';
+import translations from '../services/translations';
 
 const router = express.Router();
 
@@ -13,15 +13,17 @@ const router = express.Router();
 router.get('/', async (_req, res) => {
   const { user } = res.locals;
 
-  const results = await translation.getAllByUser(Number(user.id));
+  const results = await translations.getAllByUser(Number(user.id));
   res.json(results);
 });
 
+
 router.get('/:id', async (req, res) => {
   const translationId = Number(req.params.id);
-  const result = await translation.getOne(translationId);
+  const result = await translations.getOne(translationId);
   res.send(result);
 });
+
 
 router.get('/word/:word/', async (req, res) => {
   const { user } = res.locals;
@@ -30,9 +32,10 @@ router.get('/word/:word/', async (req, res) => {
     word: req.params.word,
   };
   const word = decodeURIComponent(data.word);
-  const result = await translation.getByWord(word, user.id);
+  const result = await translations.getByWord(word, user.id);
   res.send(result);
 });
+
 
 router.get('/word/:word/target/:targetId', async (req, res) => {
   const data = {
@@ -42,29 +45,34 @@ router.get('/word/:word/target/:targetId', async (req, res) => {
   const word = decodeURIComponent(data.word);
   const { targetId } = data;
 
-  const translationRes = await translation.getAllByWordByLang(word, targetId);
+  const translationRes = await translations.getAllByWordByLang(word, targetId);
   res.send(translationRes);
 });
 
-router.post('/', async (req, res) => {
-  const data = {
-    wordId: req.body.wordId,
-    tran: req.body.translation,
-    targetLang: req.body.targetLanguageId,
-  };
 
+router.post('/', async (req, res) => {
+  const { user } = res.locals;
   const {
-    wordId, tran, targetLang,
-  } = data;
-  const added = await translation.add(Number(wordId), tran, targetLang);
-  res.send(added);
+    wordId,
+    translation,
+    targetLanguageId,
+    context,
+  } = req.body;
+
+  const newTranslation = await translation.add(Number(wordId), translation, targetLanguageId);
+
+  await translations.addToUsersTranslations(Number(user.id), newTranslation.id, context);
+
+  res.send(newTranslation);
 });
+
 
 router.delete('/:translationId', async (req, res) => {
   const { translationId } = req.params;
-  const deleted = await translation.remove(Number(translationId));
+  const deleted = await translations.remove(Number(translationId));
   res.send(deleted);
 });
+
 
 router.put('/translation/:transId', async (req, res) => {
   const data = {
@@ -72,8 +80,9 @@ router.put('/translation/:transId', async (req, res) => {
     id: req.params.transId,
   };
   const { trans, id } = data;
-  const updated = await translation.update(trans, Number(id));
+  const updated = await translations.update(trans, Number(id));
   res.send(updated);
 });
+
 
 export default router;
