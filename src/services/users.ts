a13869adcb6c6172fd426a72471114ceee0b5e01
Marc Hermann <59806588+mrchrmn@users.noreply.github.com>
2022-01-16 16:53:55 +0100

    lower case 'test'

--- src/services/users.ts
+++ src/services/users.ts
@@ -97,7 +97,7 @@ const addNew = async function(
     await textData.addMatchGirlToUser(newUser.id, learnLanguageId);
   }
 
-  if (process.env.NODE_ENV !== 'TEST') await sendVerificationEmail(verificationCode, email, username);
+  if (process.env.NODE_ENV !== 'test') await sendVerificationEmail(verificationCode, email, username);
 
   return sanitizeUser(newUser);
 };
