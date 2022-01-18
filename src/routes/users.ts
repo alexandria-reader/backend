56c06f069dc77528e9e6573b233f5fa38a74f991
Marc Hermann <59806588+mrchrmn@users.noreply.github.com>
2022-01-18 09:36:23 +0100

    renamed deletion verificatino to confirm

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -32,12 +32,13 @@ userRouter.post('/translation/:translationId', getUserFromToken, async (req, res
   res.status(201).send();
 });
 
-userRouter.post('/verify', getUserFromToken, async (req, res) => {
+userRouter.post('/confirm', getUserFromToken, async (req, res) => {
   const { user } = res.locals;
   const { password } = req.body;
-
+  console.log(user, password);
   const response = await users.verifyPassword(user.id, password);
 
+  console.log(response);
   if (response) {
     res.json({ match: 'true' });
   } else {
