f875eb30e965adb78589fd95edc190cc9fcdbe85
Eamon <eamon0989@gmail.com>
2022-10-16 15:31:43 +0200

    test: update login tests

--- src/tests/login.test.ts
+++ src/tests/login.test.ts
@@ -23,7 +23,7 @@ beforeAll(async () => {
   await api.post('/api/users').send(newUser);
 });
 
-xdescribe('Testing user login', () => {
+describe('Testing user login', () => {
   test('users can login', async () => {
     const loginDetails = {
       password: '12345',
@@ -68,112 +68,6 @@ xdescribe('Testing user login', () => {
 
     expect(response.text).toContain('invalid email or password');
   });
-
-  // test('There is one user', async () => {
-  //   const response = await api
-  //     .get('/api/users')
-  //     .expect(200)
-  //     .expect('Content-Type', /application\/json/);
-
-  //   const responseBody = JSON.parse(response.text);
-  //   expect(responseBody).toHaveLength(1);
-  // });
-
-  // test('duplicate usernames are not added', async () => {
-  //   const newUser = {
-  //     username: 'test user',
-  //     password: '12345',
-  //     email: 'test1@userRouter.com',
-  //   };
-
-  //   const response = await api
-  //     .post('/api/users')
-  //     .send(newUser)
-  //     .expect(406)
-  //     .expect('Content-Type', /application\/json/);
-
-  //   expect(response.text).toContain('Username already in use');
-  // });
-
-  // test('duplicate emails are not added', async () => {
-  //   const newUser = {
-  //     username: 'test user1',
-  //     password: '12345',
-  //     email: 'test@userRouter.com',
-  //   };
-
-  //   const response = await api
-  //     .post('/api/users')
-  //     .send(newUser)
-  //     .expect(406)
-  //     .expect('Content-Type', /application\/json/);
-
-  //   expect(response.text).toContain('Email already in use');
-  // });
-
-  // test('users can change passwords', async () => {
-  //   const password = {
-  //     password: '12345',
-  //     newPassword: 'password',
-  //   };
-
-  //   await api
-  //     .put('/api/users/1')
-  //     .send(password)
-  //     .expect(200)
-  //     .expect('Content-Type', /application\/json/)
-  //     .expect('{"message":"Your password has been updated"}');
-  // });
-
-  // test('users cant change passwords unless correct password is supplied', async () => {
-  //   const password = {
-  //     password: '123456',
-  //     newPassword: 'password',
-  //   };
-
-  //   await api
-  //     .put('/api/users/1')
-  //     .send(password)
-  //     .expect(200)
-  //     .expect('Content-Type', /application\/json/)
-  //     .expect('{"message":"Incorrect password"}');
-  // });
-
-  // test('users cannot delete account unless correct password is supplied', async () => {
-  //   const password = {
-  //     password: 'wrong',
-  //   };
-
-  //   await api
-  //     .delete('/api/users/1')
-  //     .send(password)
-  //     .expect(200)
-  //     .expect('Content-Type', /application\/json/)
-  //     .expect('{"message":"Passwords do not match"}');
-  // });
-
-  // test('users are successfully deleted', async () => {
-  //   const password = {
-  //     password: 'password',
-  //   };
-
-  //   await api
-  //     .delete('/api/users/1')
-  //     .send(password)
-  //     .expect(200)
-  //     .expect('Content-Type', /application\/json/)
-  //     .expect('{"message":"Your account has been deleted"}');
-  // });
-
-  // test('After deletion, there are no users', async () => {
-  //   const response = await api
-  //     .get('/api/users')
-  //     .expect(200)
-  //     .expect('Content-Type', /application\/json/);
-
-  //   const responseBody = JSON.parse(response.text);
-  //   expect(responseBody).toHaveLength(0);
-  // });
 });
 
 afterAll(async () => {
